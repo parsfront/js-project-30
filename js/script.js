762eac1a96506b461cff370551048240fce1db19
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-07-13 14:15:19 +0430

    End Project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function imageSlider(anything) {
+    document.querySelector(".bmw").src = anything;
+}
+
+function changeBgColor(color) {
+    const section = document.querySelector(".section");
+    section.style.background = color;
+}
